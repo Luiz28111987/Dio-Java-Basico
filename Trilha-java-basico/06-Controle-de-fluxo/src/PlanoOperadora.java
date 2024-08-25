3492fe0d19556c859e891875d075a702201e01bc
Luiz Fernando <165337963+Luiz28111987@users.noreply.github.com>
2024-08-24 22:36:44 -0300

    fluxo

---
+++ Trilha-java-basico/06-Controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,16 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        if (plano == "B") {
+            System.out.println("100 Minutos de Ligação");
+        }else if (plano == "M") {
+            System.out.println("100 Minutos de Ligação");
+            System.out.println("Whats e Instagram gratis");
+        }else if (plano == "T") {
+            System.out.println("100 Minutos de Ligação");
+            System.out.println("Whats e Instagram gratis");
+            System.out.println("5GB de Youtube");
+        }
+    }
+}
